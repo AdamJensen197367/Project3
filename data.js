35e6541f2272b503ee4db0108f7edd6934925693
Said <zombywarfase@gmail.com>
2024-10-31 16:04:03 +0300

    Саид

---
+++ data.js
@@ -0,0 +1,40 @@
+const ARRAY_KEYS = [
+	{
+		letter: 'm',
+		color: '#91C3CE',
+		sound: 'https://oimachi.cloud/clients/mubasic/m.mp3',
+	},
+	{
+		letter: 'b',
+		color: '#FAC670',
+		sound: 'https://oimachi.cloud/clients/mubasic/b.mp3',
+	},
+	{
+		letter: 'a',
+		color: '#F5E1BE',
+		sound: 'https://oimachi.cloud/clients/mubasic/a.mp3',
+	},
+	{
+		letter: 'i',
+		color: '#F37925',
+		sound: 'https://oimachi.cloud/clients/mubasic/i.mp3',
+	},
+	{
+		letter: 'c',
+		color: '#C0A8E5',
+		sound: 'https://oimachi.cloud/clients/mubasic/c.mp3',
+	},
+]
+
+const ARRAY_KEYS2 = [
+	{
+		letter: 'u',
+		color: '#000',
+		sound: 'https://oimachi.cloud/clients/mubasic/u.mp3',
+	},
+	{
+		letter: 's',
+		color: '#000',
+		sound: 'https://oimachi.cloud/clients/mubasic/s.mp3',
+	},
+]
